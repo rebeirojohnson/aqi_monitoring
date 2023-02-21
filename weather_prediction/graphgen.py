6b495c4ce098910c6ecc2d34ec2964baa50e42e0
johnson845173 <rebeirojohnson@gmail.com>
2023-02-21 19:25:46 +0530

    added grapph

--- weather_prediction/graphgen.py
+++ weather_prediction/graphgen.py
@@ -4,7 +4,6 @@ from preprocess import x_test,y_test
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn.metrics import r2_score,accuracy_score
-x_test,y_test = x_test[:20],y_test[0:20]
 
 
 
@@ -17,20 +16,77 @@ svmlin_model = joblib.load('../pkl_file/svmlin.pkl')
 lasso_model = joblib.load('../pkl_file/lasso_model.pkl')
 ridge_model = joblib.load('../pkl_file/ridge_model.pkl')
 
-models = [random_forest_model,mlr_model,svmlin_model,svmrbf_model,lasso_model,ridge_model]
+start,stop = 0,10
+model_name = "Random Forest"
+y_pred = random_forest_model.predict(x_test[start:stop])
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("Random Forest")
+plt.legend()
+plt.show()
 
-for model in models:
-    y_pred = model.predict(x_test)
-    print(str(model))
-    print("Accuracy ",r2_score(y_test,y_pred))
-    # plt.scatter(x_test[:,1],y_test,c='red',label='original test values')
-    # plt.scatter(x_test[:,1],y_pred,c='blue',label='predicted  values')
-    print(np.append(arr=y_test.reshape(len(y_test),1),values=y_pred.reshape(len(y_pred),1),axis=1))
-    # plt.xlabel('PM2.5')d
-    # plt.ylabel('AQI')
-    # plt.title(str(model))
-    # plt.legend()
-    # plt.show()
+start,stop = 0,10
+model_name = "Multi Linear regression"
+y_pred = mlr_model.predict(x_test[start:stop])
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("Multi Linear")
+plt.legend()
+plt.show()
+
+start,stop = 0,10
+model_name = "SVR"
+y_pred = svmrbf_model.predict(x_test[start:stop])
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("svmrbf_model")
+plt.legend()
+plt.show()
+
+start,stop = 0,10
+y_pred = svmlin_model.predict(x_test[start:stop])
+model_name = "Linear SVR"
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("SVR Linear")
+plt.legend()
+plt.show()
+
+start,stop = 0,10
+model_name = "Lasso Forest"
+y_pred = lasso_model.predict(x_test[start:stop])
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("lasso Model")
+plt.legend()
+plt.show()
+
+start,stop = 0,10
+model_name = "Ridge Model"
+y_pred = ridge_model.predict(x_test[start:stop])
+print(f"Accuracy of {model_name} is {round(r2_score(y_test[start:stop],y_pred)*100,3)}%")
+plt.scatter(x_test[start:stop,1],y_test[start:stop],c='red',label='original test values')
+plt.scatter(x_test[start:stop,1],y_pred,c='blue',label='predicted  values')
+plt.xlabel('PM2.5')
+plt.ylabel('AQI')
+plt.title("ridge_model")
+plt.legend()
+plt.show()
 
 
 
