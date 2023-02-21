011d0adc8d87eff245986e6011d9e87944a91ae6
johnson845173 <rebeirojohnson@gmail.com>
2023-02-21 12:54:47 +0530

    updated the file

--- datagather.py
+++ datagather.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("dataset/horly_data_to_be_used.csv")
 
-df2 = df.dropna(thresh=3)
+print(df.isna().sum())
+
+df2 = df.dropna(thresh=7)
 
 print(df2.isna().sum())
 
-df2 = df2.dropna(thresh=7)
-
-df.to_csv("dataset/horly_data_to_be_used.csv")
+df2.to_csv("dataset/horly_data_to_be_use2.csv")
 
 
