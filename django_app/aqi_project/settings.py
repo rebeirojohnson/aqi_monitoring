1196b7e4ef91e3c0a56c03ab20e3ee2365589b5a
johnson845173 <rebeirojohnson@gmail.com>
2023-03-18 23:11:57 +0530

    removed database

--- django_app/aqi_project/settings.py
+++ django_app/aqi_project/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'aqi_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'test_db',
-        'USER': 'johnson',
-        'PASSWORD': '        ',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-} 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'test_db',
+#         'USER': 'johnson',
+#         'PASSWORD': '        ',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# } 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
