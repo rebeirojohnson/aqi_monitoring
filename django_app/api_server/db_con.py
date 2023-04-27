ceb4ba7a513f90aeabe5b3160785dc4dfaa712fe
johnson845173 <rebeirojohnson@gmail.com>
2023-04-27 22:18:12 +0530

    corrected passwor

--- django_app/api_server/db_con.py
+++ django_app/api_server/db_con.py
@@ -2,7 +2,7 @@ import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine
 
-host = r"6dpg-cftdlharrk0c835ilaj0-a.oregon-postgres.render.com"
+host = r"dpg-cftdlharrk0c835ilaj0-a.oregon-postgres.render.com"
 username = r"johnson"
 password = r"tMl2l7rHO6dQcP1xVBlmF2Wv3n0uBIcJ"
 db = "test_db"
@@ -10,7 +10,7 @@ port = 5432
 
 #Creating a cursor object using the cursor() method
 engine = create_engine(f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db}")
-# engine = engine.connect()
+engine = engine.connect()
 
 def processQuery(query: str) -> pd.DataFrame:
     conn = psycopg2.connect(
